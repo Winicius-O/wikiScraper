78e01a4562693e86048709a634b5e2f8e7d3ef35
winicius-o <winiciusoliveira3@gmail.com>
2021-06-29 22:51:11 -0300

    Novo metodo de captura

--- WebScraper.py
+++ WebScraper.py
@@ -4,9 +4,7 @@ import re
 class WebScraper:
     regexExp = {
         "urlRegex": r'\bhttps:\/\/\w+?\.wikipedia\.org\/wiki\/\S+\b',
-
-        "titleRegexTags": r'<\s*h1 id="firstHeading" class="firstHeading"\s*>\n*\t*',
-        "titleRegexContent": r'<\s*h1 id="firstHeading" class="firstHeading"\s*>\n*\t*\w+'
+        "titleRegex": r'<\s*h1 id="firstHeading" class="firstHeading"\s*>\n*\t*(\w+)'
     }
 
     def __init__(self, link) -> None:
@@ -17,15 +15,5 @@ class WebScraper:
             raise Exception("[*] URL inválido")
 
     def getTitle(self) -> str:
-        # a informação buscada vem dessa maneira
-        # <h1 id="firstHeading" class="firstHeading">
-		# 	Jornalismo
-		# </h1>
-    
-        #guarda resultado até o final da formatação html
-        tag = re.findall(self.regexExp["titleRegexTags"], self.htmlContent)
-        #guarda a formatação html + conteudo da tag
-        content = re.findall(self.regexExp["titleRegexContent"], self.htmlContent)
-        
-        #seu retorno é basicamente a diferença entre as duas regex
-        return content[0][len(tag[0]):]
+        # retorna o grupo de captura contido no match
+        return re.findall(self.regexExp["titleRegex"], self.htmlContent)[0]
