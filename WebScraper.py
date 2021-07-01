27829f5c816666ab7ee4b55e96ee65a4b3de931d
winicius-o <winiciusoliveira3@gmail.com>
2021-07-01 00:25:02 -0300

    usando biblioteca BeautifulSoup para formatação do arquivo html

--- WebScraper.py
+++ WebScraper.py
@@ -1,17 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
 import re
 
 class WebScraper:
     regexExp = {
         "urlRegex": r'\bhttps:\/\/\w+?\.wikipedia\.org\/wiki\/\S+\b',
-        "titleRegex": r'<\s*h1 id="firstHeading" class="firstHeading"\s*>\n*\t*(.+)',
-        "topicsRegex": r'<\s*span class="toctext"\s*>\n*\t*(.+)\n*\t*<\s*/span\s*>'
+        "titleRegex": r'<h1 class="firstHeading" id="firstHeading">[\n\s]*(.+)[\n\s]*</h1>',
+        "topicsRegex": r'<span class="toctext">[\n\s]*(.+)[\n\s]*<\/span>',
+        "imgRegex": r''
     }
 
     def __init__(self, link) -> None:
         if re.match(self.regexExp["urlRegex"], link):
             self.url = link
-            self.htmlContent = requests.get(link).text
+            requestsContent = requests.get(link).text
+            self.htmlContent = BeautifulSoup(requestsContent, 'html.parser').prettify()
         else: #re.match == None
             raise Exception("[*] URL inválido")
 
@@ -20,4 +23,17 @@ class WebScraper:
         return re.findall(self.regexExp["titleRegex"], self.htmlContent)[0]
 
     def getTopics(self) -> list:
-        return re.findall(self.regexExp["topicsRegex"], self.htmlContent)
+        content = re.findall(self.regexExp["topicsRegex"], self.htmlContent)
+
+        if len(content)!=0:
+            return content
+        else:
+            return None
+
+    def getImageDesc(self) -> list:
+        content = re.findall(self.regexExp["imgRegex"], self.htmlContent)
+
+        if len(content)!=0:
+            return content
+        else:
+            return None
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from WebScraper import WebScraper
 
-link = "https://pt.wikipedia.org/wiki/Jornalismo"
+link = "https://pt.wikipedia.org/wiki/jornalismo"
 x = WebScraper(link)
 
+# with open('pretty.html', 'w', encoding='UTF-8') as f:
+#     f.write(x.htmlContent)
+
 print(x.getTitle())
+print(x.getTopics())
